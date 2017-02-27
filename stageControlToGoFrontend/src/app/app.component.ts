95690d8410bebb5029e0e83c51f5db9702aa6b23
Simon Witzke <witzke.simon@gmail.com>
2017-02-27 13:35:22 +0100

    Add basic css

--- stageControlToGoFrontend/src/app/app.component.ts
+++ stageControlToGoFrontend/src/app/app.component.ts
@@ -5,12 +5,12 @@ import { ScriptProviderService, MOCKSCRIPT } from './script-provider.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
 
   providers: [ScriptProviderService]
 })
 export class AppComponent {
-  title = 'app works!2';
+  title = 'Studienarbeit Mock';
   script = MOCKSCRIPT;
 
   constructor(private scriptProviderService:ScriptProviderService) {
